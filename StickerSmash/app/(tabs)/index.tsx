f92e626e7206cc53b08015278e4f6935ab40e60d
Divyansh <gdeva368@gmail.com>
2024-11-09 15:05:57 +0530

    Added the imagePicker functionality

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -2,19 +2,37 @@ import {  View } from "react-native";
 import { StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/button";
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
 
 
 const PlaceholderImage = require('../../assets/images/background-image.png');
 
 export default function Index(){
+
+  const[selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async()=>{
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true, //this will allow the user to crop the image
+      quality: 1,
+    });
+
+    if(!result.canceled){
+      setSelectedImage(result.assets[0].uri); //this will store the uri of the selected image under the selectedImage varialble
+    } else{
+      alert('You did not select any image.');
+    }
+  }
+
   return(
     <View style={styles.container}>
       <View style= {styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage}/>
+        <ImageViewer imgSource={PlaceholderImage} />
       </View>
 
       <View style={styles.footContainer}>
-        <Button label={"Choose a photo"} theme="primary"/>
+        <Button label={"Choose a photo"} theme="primary" onPress={pickImageAsync}/>
         <Button label={"Use this photo"}/>
       </View>
 
--- StickerSmash/components/button.tsx
+++ StickerSmash/components/button.tsx
@@ -5,9 +5,10 @@ import { FontAwesome } from "@expo/vector-icons";
 type props={
     label: String;
     theme?:'primary'; //this question mark means theme is not compulsary to be provided and the primary here means the input can only be primary
+    onPress?: ()=>void;
 }
 
-export default function Button({label, theme}:props){
+export default function Button({label, theme, onPress}:props){
     if(theme ==='primary'){
         return(
             <View
@@ -17,9 +18,7 @@ export default function Button({label, theme}:props){
             ]}>
                 <Pressable
                 style={[styles.button, {backgroundColor: '#fff'}]}
-                onPress={()=>{
-                    alert('You pressed a button.')
-                }}>
+                onPress={onPress}>
                     <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon}/>
                     <Text style={[styles.buttonlabel, {color: '#25292e'}]}>{label}</Text>
                 </Pressable>
